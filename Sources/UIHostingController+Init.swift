40e699e4a8be4f1443199a6af8ee2238a79c10c1
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:00:04 +0300

    add UIHostingController+Init file

---
+++ Sources/UIHostingController+Init.swift
@@ -0,0 +1,14 @@
+/*
+
+Project: SwiftUIRouter
+File: UIHostingController+Init.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+extension UIHostingController where Content == AnyView {}
