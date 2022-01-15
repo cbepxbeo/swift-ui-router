9e077bc626f8f665cd466717716d023038759c12
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:03:18 +0300

    add RouterViewWrapper file

---
+++ Sources/RouterViewWrapper.swift
@@ -0,0 +1,14 @@
+/*
+
+Project: SwiftUIRouter
+File: RouterViewWrapper.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+public struct RouterViewWrapper {}
