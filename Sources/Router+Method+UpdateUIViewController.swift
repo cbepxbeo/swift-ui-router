465e4f1e0d0fb34453f69d4a8458e1accf79f8f7
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:02:03 +0300

    add Router+Method+UpdateUIViewController file

---
+++ Sources/Router+Method+UpdateUIViewController.swift
@@ -0,0 +1,15 @@
+/*
+
+Project: SwiftUIRouter
+File: Router+Method+UpdateUIViewController.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+
+extension Router {}
