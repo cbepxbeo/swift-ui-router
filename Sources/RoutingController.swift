f8e82d0ec9bc219044a03ed2df186b7bf58264a7
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:03:28 +0300

    add RoutingController file

---
+++ Sources/RoutingController.swift
@@ -0,0 +1,14 @@
+/*
+
+Project: SwiftUIRouter
+File: RoutingController.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+open class RoutingController: NSObject, RouterController {}
