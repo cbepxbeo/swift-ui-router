a4127736e76a7f667a37e99286dc5abe29f2852c
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 13:09:48 +0300

    add method makeUIViewController to router

--- Sources/Router+Method+MakeUIViewController.swift
+++ Sources/Router+Method+MakeUIViewController.swift
@@ -11,4 +11,29 @@ Status: #Complete | #Not decorated
 
 import SwiftUI
 
-extension Router {}
+extension Router {
+    @MainActor public func makeUIViewController(context: Context) -> UINavigationController {
+        UINavigationBar.appearance().tintColor = .clear
+        
+        let rootView = UIHostingController(
+            view: self.content,
+            controller: self.routingController
+        )
+        
+        self.logger.debug("Setting up UINavigationController")
+        self.logger.debug("UIHostingController complited")
+        self.navigationController.delegate = self.navigationDelegate
+        self.logger.debug("UINavigationController: Delegate setup completed")
+        self.logger.debug("Navigation bar customization")
+        self.navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController.navigationBar.shadowImage = UIImage()
+        self.navigationController.navigationBar.isTranslucent = true
+        self.pushToNavigation(controller: self.navigationController, toPush: rootView)
+        self.logger.debug("Navigation bar customization completed")
+        self.logger.debug("Setting up gesture processing")
+        self.navigationController.interactivePopGestureRecognizer?.delegate = self.recognizerDelegate
+        self.logger.debug("Setting completed")
+        return self.navigationController
+    }
+    
+}
