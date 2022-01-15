cf87edd33de3aaec40ab227c3c233eedb05f5968
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 13:08:57 +0300

    add properties, base init to router

--- Sources/Router.swift
+++ Sources/Router.swift
@@ -13,4 +13,26 @@ import SwiftUI
 import SwiftUtilities
 import SwiftUIExtension
 
-public struct Router<Content: View, Controller: RouterController>: UIViewControllerRepresentable {}
+public struct Router<Content: View, Controller: RouterController>: UIViewControllerRepresentable {
+    public init(controllerType: Controller.Type, _ content: @escaping () -> Content){
+        self.logger = LoggerProvider.default(category: "Router")
+        self.content = content
+        self._routingController = StateObject(initialValue: { .init(rootView: nil) })
+        self.navigationController = UINavigationController()
+        self.recognizerDelegate = .init()
+        self.navigationDelegate = .init()
+        self.recognizerDelegate.setNavigationController(self.navigationController)
+        self.logger.debug("Initialized")
+    }
+    
+    internal let _routingController: StateObject<Controller>
+    internal let logger: LoggerProvider.LoggerWrapper
+    internal let content: () -> Content
+    internal let navigationController: UINavigationController
+    internal let recognizerDelegate:  Self.RecognizerDelegate
+    internal let navigationDelegate: Self.NavigationDelegate
+    
+    internal var routingController: Controller {
+        self._routingController.wrappedValue
+    }
+}
