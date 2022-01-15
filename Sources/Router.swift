345cee9e6000f58d7158edda0c88e05b1d67cf69
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:00:19 +0300

    add Router file

---
+++ Sources/Router.swift
@@ -0,0 +1,16 @@
+/*
+
+Project: SwiftUIRouter
+File: Router.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+import SwiftUtilities
+import SwiftUIExtension
+
+public struct Router<Content: View, Controller: RouterController>: UIViewControllerRepresentable {}
