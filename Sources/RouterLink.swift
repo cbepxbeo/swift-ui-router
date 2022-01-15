ea70e547311f5629fa76a1f8299e3f82e9143f5b
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:03:08 +0300

    add RouterLink file

---
+++ Sources/RouterLink.swift
@@ -0,0 +1,15 @@
+/*
+
+Project: SwiftUIRouter
+File: RouterLink.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+public struct RouterLink<Content: View, Label: View>: View {}
+
