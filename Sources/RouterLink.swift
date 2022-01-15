35bf32220d0ac766a1081e571ab7d698799d896f
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 13:13:15 +0300

    add properties and base init to RouterLink

--- Sources/RouterLink.swift
+++ Sources/RouterLink.swift
@@ -11,5 +11,30 @@ Status: #Complete | #Not decorated
 
 import SwiftUI
 
-public struct RouterLink<Content: View, Label: View>: View {}
+public struct RouterLink<Content: View, Label: View>: View {
+    
+    @EnvironmentObject var routingController: RoutingController
+    
+    public init(
+        _ label: @escaping () -> Label,
+        _ destination: @escaping () -> Content) {
+            self.label = label
+            self.destination = destination
+        }
+    
+    private let label: () -> Label
+    private let destination: () -> Content
+
+    public var body: some View {
+        Button(
+            action: action,
+            label: label
+        )
+    }
+    
+    func action(){
+        self.routingController.push(self.destination)
+    }
+    
+}
 
