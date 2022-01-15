c827e0b2b29a07cdf06b3ed090ceebb596b217b3
cbepxbeo <cbepxbeo@mail.ru>
2022-01-15 12:02:45 +0300

    add RouterController file

---
+++ Sources/RouterController.swift
@@ -0,0 +1,14 @@
+/*
+
+Project: SwiftUIRouter
+File: RouterController.swift
+Created by: Egor Boyko
+Date: 15.01.2022
+
+Status: #Complete | #Not decorated
+
+*/
+
+import SwiftUI
+
+public protocol RouterController: ObservableObject {}
