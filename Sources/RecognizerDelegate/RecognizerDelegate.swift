b30300d651ab8548cc1fc0e264b61ab1d6f26f5d
cbepxbeo <cbepxbeo@mail.ru>
2023-06-02 20:31:00 +0300

    add RecognizerDelegate class

---
+++ Sources/RecognizerDelegate/RecognizerDelegate.swift
@@ -0,0 +1,16 @@
+/*
+
+Project: SwiftUIRouter
+File: RecognizerDelegate.swift
+Created by: Egor Boyko
+Date: 02.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+import UIKit
+
+internal final class RecognizerDelegate: NSObject {
+    internal weak var navigationController: UINavigationController?
+}
