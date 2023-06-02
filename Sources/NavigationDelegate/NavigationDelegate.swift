c5eafba5b8cbd413dba89628d2775c6daee73d17
cbepxbeo <cbepxbeo@mail.ru>
2023-06-02 20:26:46 +0300

    add NavigationDelegate class

---
+++ Sources/NavigationDelegate/NavigationDelegate.swift
@@ -0,0 +1,14 @@
+/*
+
+Project: SwiftUIRouter
+File: NavigationDelegate.swift
+Created by: Egor Boyko
+Date: 02.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+import ObjectiveC
+
+internal final class NavigationDelegate: NSObject {}
