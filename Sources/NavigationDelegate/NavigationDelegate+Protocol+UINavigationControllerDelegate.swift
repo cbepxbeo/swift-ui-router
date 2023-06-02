025c76976f084c5321247424a6b8cd9ea49428a0
cbepxbeo <cbepxbeo@mail.ru>
2023-06-02 20:29:09 +0300

    NavigationDelegate + protocol UINavigationControllerDelegate -method navigationController (willShow) -method navigationController (didShow)

---
+++ Sources/NavigationDelegate/NavigationDelegate+Protocol+UINavigationControllerDelegate.swift
@@ -0,0 +1,30 @@
+/*
+
+Project: SwiftUIRouter
+File: NavigationDelegate+Protocol+UINavigationControllerDelegate.swift
+Created by: Egor Boyko
+Date: 02.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+import UIKit
+
+extension NavigationDelegate: UINavigationControllerDelegate {
+    @MainActor internal func navigationController(
+        _ navigationController: UINavigationController,
+        willShow viewController: UIViewController,
+        animated: Bool
+    ){
+        navigationController.setNavigationBarHidden(true, animated: false)
+    }
+    
+    @MainActor internal func navigationController(
+        _ navigationController: UINavigationController,
+        didShow viewController: UIViewController,
+        animated: Bool
+    ){
+        navigationController.setNavigationBarHidden(true, animated: false)
+    }
+}
