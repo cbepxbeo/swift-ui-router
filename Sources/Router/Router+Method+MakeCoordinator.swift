e20cee375c3b003925bbd85c5bb361ef31e30e4f
cbepxbeo <cbepxbeo@mail.ru>
2023-06-03 03:23:42 +0300

    fix method makeCoordinator (Router)

--- Sources/Router/Router+Method+MakeCoordinator.swift
+++ Sources/Router/Router+Method+MakeCoordinator.swift
@@ -4,13 +4,14 @@ Project: SwiftUIRouter
 File: Router+Method+MakeCoordinator.swift
 Created by: Egor Boyko
 Date: 15.01.2022
+Last Fix: 02.06.2023
 
 Status: #Complete | #Not decorated
 
 */
 
 extension Router {
-    public func makeCoordinator() -> Controller {
+    public func makeCoordinator() -> RoutingController {
         self.routingController
     }
 }
