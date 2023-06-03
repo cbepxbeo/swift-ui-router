fe9fdb63ec12fae98ce2139677a494b75ef6fd1c
cbepxbeo <cbepxbeo@mail.ru>
2023-06-03 15:20:43 +0300

    update method makeCoordinator (Router) - to install the controller in an external controller

--- Sources/Router/Router+Method+MakeCoordinator.swift
+++ Sources/Router/Router+Method+MakeCoordinator.swift
@@ -4,7 +4,7 @@ Project: SwiftUIRouter
 File: Router+Method+MakeCoordinator.swift
 Created by: Egor Boyko
 Date: 15.01.2022
-Last Fix: 02.06.2023
+Last Fix: 03.06.2023
 
 Status: #Complete | #Not decorated
 
@@ -12,6 +12,7 @@ Status: #Complete | #Not decorated
 
 extension Router {
     public func makeCoordinator() -> RoutingController {
-        self.routingController
+        self.externalRoutingController?.routingController = self.routingController
+        return self.routingController
     }
 }
