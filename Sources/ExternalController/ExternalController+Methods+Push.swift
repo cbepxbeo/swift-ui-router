2ea82dd467a1995aeb9009fa52d5c56398dde7ba
Egor Olegovich <cbepxbeo@mail.ru>
2023-06-03 21:03:28 +0300

    update method push to ExternalController

--- Sources/ExternalController/ExternalController+Methods+Push.swift
+++ Sources/ExternalController/ExternalController+Methods+Push.swift
@@ -12,17 +12,20 @@ Status: #Complete | #Not decorated
 import SwiftUI
 
 extension ExternalController {
-    @MainActor public func push<Destination: View>(
+    @MainActor public func push<Destination: View, Tag: Hashable>(
+        tag: Tag?  = Optional<Int>.none,
         @ViewBuilder _ content: @escaping () -> Destination){
-            self.routingController?.push(nil, nil, nil, content)
+            self.routingController?.push(tag, nil, nil, nil, content)
         }
     
-    @MainActor public func push<Destination: View>(
+    @MainActor public func push<Destination: View, Tag: Hashable>(
+        tag: Tag?  = Optional<Int>.none,
         transition subType: CATransitionSubtype,
         type: CATransitionType = .push,
         duration: CGFloat = 0.3,
         @ViewBuilder _ content: @escaping () -> Destination){
             self.routingController?.push(
+                tag,
                 subType,
                 type,
                 duration,
