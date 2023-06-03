1e7833b5e4024c8a2a2c34efd1622e83f1bdb3a0
Egor Olegovich <cbepxbeo@mail.ru>
2023-06-03 21:02:49 +0300

    update RoutingController (add storage)

--- Sources/RoutingController/RoutingController.swift
+++ Sources/RoutingController/RoutingController.swift
@@ -4,7 +4,7 @@ Project: SwiftUIRouter
 File: RoutingController.swift
 Created by: Egor Boyko
 Date: 15.01.2022
-Last Fix: 02.06.2023
+Last Fix: 03.06.2023
 
 Status: #Complete | #Not decorated
 
@@ -17,9 +17,11 @@ public final class RoutingController: NSObject, ObservableObject {
     required public override init(){
         self.navigationDelegate = .init()
         self.recognizerDelegate = .init()
+        self.storage = [:]
         super.init()
     }
     
+    internal var storage: [Int: RouterWeakStorage<UIViewController>]
     internal var navigationController: UINavigationController?
     internal let navigationDelegate: NavigationDelegate
     internal let recognizerDelegate: RecognizerDelegate
