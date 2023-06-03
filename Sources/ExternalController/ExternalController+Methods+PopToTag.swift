4e8c0f82c09de6ebc372cdbfc1507076564908f1
Egor Olegovich <cbepxbeo@mail.ru>
2023-06-03 21:01:57 +0300

    add methods popToTag to ExternalController

---
+++ Sources/ExternalController/ExternalController+Methods+PopToTag.swift
@@ -0,0 +1,26 @@
+/*
+
+Project: SwiftUIRouter
+File: ExternalController+Methods+PopToTag.swift
+Created by: Egor Boyko
+Date: 03.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+import UIKit
+
+extension ExternalController {
+    @MainActor public func popToTag<Tag: Hashable>(
+        tag: Tag,
+        transition subType: CATransitionSubtype,
+        type: CATransitionType = .push,
+        duration: CGFloat = 0.3) throws {
+            try self.routingController?.popToTag(tag, subType, type, duration)
+        }
+    
+    @MainActor public func popToTag<Tag: Hashable>(tag: Tag) throws {
+        try self.routingController?.popToTag(tag, nil, nil, nil)
+    }
+}
