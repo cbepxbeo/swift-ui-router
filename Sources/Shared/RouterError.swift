17599dbc820d1dcdfccf7b06404bcdb70481741f
Egor Olegovich <cbepxbeo@mail.ru>
2023-06-03 20:58:12 +0300

    add RouterError

---
+++ Sources/Shared/RouterError.swift
@@ -0,0 +1,15 @@
+/*
+
+Project: SwiftUIRouter
+File: RouterError.swift
+Created by: Egor Boyko
+Date: 03.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+
+public enum RouterError: Error {
+    case unknownTag
+}
