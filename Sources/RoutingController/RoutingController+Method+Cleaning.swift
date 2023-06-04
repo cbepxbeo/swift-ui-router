89341ccb13097f8d773f1b869db3cbec8caca98c
cbepxbeo <cbepxbeo@mail.ru>
2023-06-04 15:41:31 +0300

    Create RoutingController+Method+Cleaning.swift
    add method cleaning

---
+++ Sources/RoutingController/RoutingController+Method+Cleaning.swift
@@ -0,0 +1,33 @@
+/*
+
+Project: SwiftUIRouter
+File: RoutingController+Method+Cleaning.swift
+Created by: Egor Boyko
+Date: 03.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+import UIKit
+
+extension RoutingController {
+    
+    internal func cleaning(){
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let routingController = self else {
+                return
+            }
+            let keys = routingController.storage.compactMap { $0.value.value == nil ? $0.key : nil  }
+            if keys.isEmpty {
+                return
+            }
+            DispatchQueue.main.async {
+                keys.forEach {
+                    routingController.storage[$0] = nil
+                }
+            }
+        }
+    }
+    
+}
