584b880a1955425134f0d68e18a6bc159092f2e2
cbepxbeo <cbepxbeo@mail.ru>
2023-06-04 15:41:14 +0300

    Update RouterLink.swift

--- Sources/RouterLink/RouterLink.swift
+++ Sources/RouterLink/RouterLink.swift
@@ -12,17 +12,20 @@ Status: #Complete | #Not decorated
 
 import SwiftUI
 
-public struct RouterLink<Content: View, Label: View>: View {
+public struct RouterLink<Content: View, Label: View, Tag: Hashable>: View {
     
     @EnvironmentObject internal var routingController: RoutingController
     
     public init(
+        tag: Tag,
         _ label: @escaping () -> Label,
         _ destination: @escaping () -> Content) {
             self.label = label
             self.destination = destination
+            self.tag = tag
         }
-    
+
+    internal let tag: Tag?
     internal let label: () -> Label
     internal let destination: () -> Content
 
