1586b40012af6527c54f2b84cf2fe35d6494330f
cbepxbeo <cbepxbeo@mail.ru>
2023-06-04 15:42:15 +0300

    Create RouterLinkOption.swift

---
+++ Sources/Shared/RouterLinkOption.swift
@@ -0,0 +1,15 @@
+/*
+
+Project: SwiftUIRouter
+File: RouterLinkOption.swift
+Created by: Egor Boyko
+Date: 03.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+public enum RouterLinkOption {
+    case back
+    case toRootView
+}
