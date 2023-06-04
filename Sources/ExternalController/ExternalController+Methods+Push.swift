85e856fcfc270fba7398c10af31d5339fd240d2c
cbepxbeo <cbepxbeo@mail.ru>
2023-06-04 15:39:14 +0300

    update ExternalController
    methods push (to tag)

--- Sources/ExternalController/ExternalController+Methods+Push.swift
+++ Sources/ExternalController/ExternalController+Methods+Push.swift
@@ -12,14 +12,36 @@ Status: #Complete | #Not decorated
 import SwiftUI
 
 extension ExternalController {
+    
     @MainActor public func push<Destination: View, Tag: Hashable>(
-        tag: Tag?  = Optional<Int>.none,
-        @ViewBuilder _ content: @escaping () -> Destination){
-            self.routingController?.push(tag, nil, nil, nil, content)
+        tag: Tag,
+        @ViewBuilder _ content: @escaping () -> Destination) throws {
+            try self.routingController?.push(tag: tag, content)
         }
     
     @MainActor public func push<Destination: View, Tag: Hashable>(
-        tag: Tag?  = Optional<Int>.none,
+        replacingTag tag: Tag?  = Optional<Int>.none,
+        @ViewBuilder _ content: @escaping () -> Destination){
+            self.routingController?.push(replacingTag: tag, content)
+        }
+    
+    @MainActor public func push<Destination: View, Tag: Hashable>(
+        tag: Tag,
+        transition subType: CATransitionSubtype,
+        type: CATransitionType = .push,
+        duration: CGFloat = 0.3,
+        @ViewBuilder _ content: @escaping () -> Destination) throws {
+            try self.routingController?.push(
+                tag: tag,
+                transition: subType,
+                type: type,
+                duration: duration,
+                content
+            )
+        }
+    
+    @MainActor public func push<Destination: View, Tag: Hashable>(
+        replacingTag tag: Tag?  = Optional<Int>.none,
         transition subType: CATransitionSubtype,
         type: CATransitionType = .push,
         duration: CGFloat = 0.3,
